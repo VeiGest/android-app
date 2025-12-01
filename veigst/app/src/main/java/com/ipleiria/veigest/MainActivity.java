3ae592d17c72a9da79c51547b01ea0488e9aeb61
Edo Santos <2231440@my.ipleiria.pt>
2025-12-01 16:20:30 +0000

    Login / Light & Dark Themes
    Created the new Login layout;
    Created attrs.xml with custom color attributes;
    Linked attributes to palette colors in both Light and Dark themes;
    Updated UI layouts to resolve colors via theme attributes;
    Removed hardcoded @color usage from components;
    Improved theme consistency and simplified future design changes.

--- veigst/app/src/main/java/com/ipleiria/veigest/MainActivity.java
+++ veigst/app/src/main/java/com/ipleiria/veigest/MainActivity.java
@@ -7,6 +7,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,5 +16,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Só adiciona o fragment se for a primeira criação
+        if (savedInstanceState == null) {
+            LoginFragment loginFragment = new LoginFragment();
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.fragment_container, loginFragment);
+            fragmentTransaction.commit();
+        }
     }
 }
