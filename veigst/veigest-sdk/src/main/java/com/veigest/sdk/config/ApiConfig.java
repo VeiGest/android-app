2a23b4664142fdf19f2c6dde034d019343024ba6
Edo Santos <2231440@my.ipleiria.pt>
2026-01-21 23:51:22 +0000

    Unnecessary Files Deleted
    /App/ApiConfig
    Tests

--- veigst/veigest-sdk/src/main/java/com/veigest/sdk/config/ApiConfig.java
+++ veigst/veigest-sdk/src/main/java/com/veigest/sdk/config/ApiConfig.java
@@ -2,5 +2,7 @@ package com.veigest.sdk.config;
 
 public class ApiConfig {
     // Troque este endereço conforme o ambiente (emulador, produção, etc)
-    public static final String API_BASE_URL = "http://10.0.2.2/android-app/Website/website-VeiGest/veigest/backend/web/api";
+    //public static final String API_BASE_URL = "http://10.0.2.2/android-app/Website/website-VeiGest/veigest/backend/web/api";
+    public static final String API_BASE_URL = "http://veigestback.dryadlang.org/api/";
+
 }
